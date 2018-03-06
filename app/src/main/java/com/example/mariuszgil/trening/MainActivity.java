c59b093989e4831ce6cb67ea4afc385baa03a2e3
mariuszgil <mariusz.gil91@gmail.com>
2018-03-06 20:57:11 +0100

    new buttons

--- app/src/main/java/com/example/mariuszgil/trening/MainActivity.java
+++ app/src/main/java/com/example/mariuszgil/trening/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.mariuszgil.trening;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /** Called when the user taps the Send button */
+    public void getInTrening(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, TreningView.class);
+        startActivity(intent);
+    }
+    public void getInHistoria(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, HistoriaView.class);
+        startActivity(intent);
+    }
+
 }
