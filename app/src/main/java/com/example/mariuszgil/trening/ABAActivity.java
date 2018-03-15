c833d77aec6397ebeca5c0b76ed65ebb42012ad7
mariuszgil <mariusz.gil91@gmail.com>
2018-03-15 21:03:38 +0100

    plus/minus buttons (need to be fixed)

--- app/src/main/java/com/example/mariuszgil/trening/ABAActivity.java
+++ app/src/main/java/com/example/mariuszgil/trening/ABAActivity.java
@@ -7,15 +7,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ABAActivity extends AppCompatActivity {
+
+    int minteger = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aba);
 
+    }
+    public void increaseInteger(View view) {
+        minteger = minteger + 1;
+        display(minteger);
 
+    }
+    public void decreaseInteger(View view) {
+        minteger = minteger - 1;
+        display(minteger);
+    }
+
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(
+                R.id.integer_number);
+        displayInteger.setText("" + number + "kg");
     }
 
 }
